5a37a594bba98328dc7ec829a4a0c1d2fed271d8
Barry Silliman <silliman@Barrys-MacBook-Pro.local>
2018-12-17 17:14:43 -0500

    correct directory path

--- VehicleSetup.sh
+++ VehicleSetup.sh
@@ -4,6 +4,6 @@ sudo apt-get -y update
 sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common python-pip build-essential libltdl3-dev unzip
 
 #Setup and launch demo
-cd Z-vehicle-demo/packages/vehicle-lifecycle
+cd packages/vehicle-lifecycle
 ./build.sh 
 cat installers/hlfv11/install.sh | bash
